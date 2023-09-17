d532622d3ebab2022a1ec298c65129c7d343282b
TatyyanaS <taty.yana.kz@gmail.com>
2023-09-17 19:46:01 +0600

    feat: do

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    let a = Math.abs(n).toString().split('');
+    if(a[a.length-1] == '0') {
+        a.splice(a.length-1, 1);
+    }
+    a.reverse().toString();
+    return Number(a.join(''));
+};
